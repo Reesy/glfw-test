cdf5ca72d98cbb484f7fbf38d24086aab3040113
James Rees <james.rees@outlook.com>
2015-02-10 23:29:04 +0000

    Added a state setting function, glClearColor(x,x,x,x); and a State using function in terms of glClear

--- glfwTest2/main.cpp
+++ glfwTest2/main.cpp
@@ -42,10 +42,11 @@ int main(void)
 
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
-
-
-
     }
 
     glfwTerminate;
