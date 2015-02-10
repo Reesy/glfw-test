5682ba537a1041f90eb2798afc8ed1f8f56abfb7
James Rees <james.rees@outlook.com>
2015-02-10 23:23:08 +0000

    Renders blank screen and can takes input

--- glfwTest2/main.cpp
+++ glfwTest2/main.cpp
@@ -1,39 +1,59 @@
 // GLEW
+#include <iostream>
 #define GLEW_STATIC
 #include <GL/glew.h>
+// GLFW
 #include <GLFW/glfw3.h>
 
+
+//forward declaration
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 int main(void)
 {
-    GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOPENGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+    if(window == NULL)
     {
+
+        std::cout << "Failed to create window!!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
     {
-        /* Render here */
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
     }
 
-    glfwTerminate();
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+
+
+
+    }
+
+    glfwTerminate;
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode){
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
