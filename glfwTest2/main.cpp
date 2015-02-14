c67de05a65c729e9fa3e10c50ea04216891d5e0a
James Rees <james.rees@outlook.com>
2015-02-14 11:38:25 +0000

    Made code more readable and changed back to traingle

--- glfwTest2/main.cpp
+++ glfwTest2/main.cpp
@@ -51,46 +51,45 @@ int main()
     glViewport(0, 0, 800, 600);
 
     // Build and compile our shader program
+    //vertex shader
     GLint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    GLint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    GLint shaderProgram = glCreateProgram();
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
+
+    //fragment shader
+    GLint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+
+    //attach shader to shader program
+    GLint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+
+    //clear up memory
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
     // Set up our vertex data (and buffer(s)) and attribute pointers
     GLfloat vertices[] = {
-        0.5f, 0.5f,	   // Top Right
-        0.5f, -0.5f,   // Bottom Right
-        -0.5f, -0.5f,  // Bottom Left
-        -0.5f, 0.5f	   // Top Left
+        0.5f, -0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
     };
 
-    GLuint indices[] = {
-        0, 1, 3,
-        1, 2 ,3
-
-    };
-    GLuint VBO, VAO, EBO;
+    GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+
     // Bind our Vertex Array Object first, then bind and set our buffers and pointers.
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+    //position attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
@@ -119,7 +118,7 @@ int main()
 
         //Draw the triangle
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
 
         // Swap the buffers
